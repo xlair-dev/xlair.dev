9b57af3d246177963712f7a69237b1b9779a57eb
n4mlz <n4mlz.dev@gmail.com>
2025-10-31 20:01:22 +0900

    :sparkles: Noto Sans JP と Jost を導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,20 @@
 import type { Metadata } from "next";
+import { Noto_Sans_JP, Jost } from "next/font/google";
 import "./globals.css";
+
+const notoSansJP = Noto_Sans_JP({
+	subsets: ["latin"],
+	weight: ["400", "500", "700"],
+	variable: "--font-noto-sans-jp",
+	display: "swap",
+});
+
+const jost = Jost({
+	subsets: ["latin"],
+	weight: ["400", "500", "700"],
+	variable: "--font-jost",
+	display: "swap",
+});
 
 export const metadata: Metadata = {
 	title: "xlair.dev",
@@ -11,8 +26,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="ja">
-			<body>{children}</body>
+		<html lang="ja" className={`${notoSansJP.variable} ${jost.variable}`}>
+			<body className={notoSansJP.className}>{children}</body>
 		</html>
 	);
 }
