05fe6703a4a36776da5ff3bd2b4a1112a222cc5d
n4mlz <n4mlz.dev@gmail.com>
2025-10-31 21:41:05 +0900

    :adhesive_bandage: 三角形の大きさを調整

--- app/page.tsx
+++ app/page.tsx
@@ -34,10 +34,10 @@ export default function Home() {
 				className="w-[90vw] sm:w-[80vw] h-auto max-w-[1080px]"
 				priority
 			/>
-				<div className="text-brand-main font-medium mt-4 sm:mt-6 flex flex-col sm:flex-row items-center gap-0 sm:gap-2 md:gap-3">
+				<div className="text-brand-main font-medium mt-4 sm:mt-6 flex flex-col item-end sm:flex-row items-center gap-0 sm:gap-2 md:gap-3">
 					<div className="flex flex-row items-center text-3xl sm:text-3xl md:text-4xl lg:text-5xl">
 						<p>11.02</p>
-						<NavigationArrow className="text-brand-main-light w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7" />
+						<NavigationArrow className="text-brand-main-light w-8 h-8 sm:w-8 sm:h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />
 						<p>11.03</p>
 					</div>
 					<div className="flex flex-row items-end text-base sm:text-xl md:text-2xl lg:text-3xl">
