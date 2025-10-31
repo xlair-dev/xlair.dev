ae73258663bdea4ffeb62ba63886cbfd55483a76
n4mlz <n4mlz.dev@gmail.com>
2025-10-31 20:02:19 +0900

    :sparkles: 三角形のナビゲーションをコンポーネントに

---
+++ components/navigation-arrow.tsx
@@ -0,0 +1,37 @@
+import type { ComponentPropsWithoutRef } from "react";
+
+interface NavigationArrowProps
+	extends Omit<ComponentPropsWithoutRef<"svg">, "viewBox"> {
+	/**
+	 * Size of the arrow icon in pixels or Tailwind size class
+	 * @default "1em"
+	 */
+	size?: string | number;
+}
+
+/**
+ * Right-pointing triangle arrow for navigation.
+ * Designed to be placed between text elements with customizable size.
+ */
+export default function NavigationArrow({
+	size = "1em",
+	className = "",
+	...props
+}: NavigationArrowProps) {
+	const sizeValue = typeof size === "number" ? `${size}px` : size;
+
+	return (
+		<svg
+			width={sizeValue}
+			height={sizeValue}
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			className={`inline-block ${className}`}
+			aria-hidden="true"
+			{...props}
+		>
+			<path d="M10 6L16 12L10 18" fill="currentColor" />
+		</svg>
+	);
+}
