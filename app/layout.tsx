32bc5d5790191612938ce718cddd815266dafbe6
n4mlz <n4mlz.dev@gmail.com>
2025-11-01 22:40:47 +0900

    :zap: font-weight を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,14 @@ import "./globals.css";
 
 const notoSansJP = Noto_Sans_JP({
 	subsets: ["latin"],
-	weight: ["400", "500", "700"],
+	weight: ["200", "300", "400", "500", "700"],
 	variable: "--font-noto-sans-jp",
 	display: "swap",
 });
 
 const jost = Jost({
 	subsets: ["latin"],
-	weight: ["400", "500", "700"],
+	weight: ["200", "300", "400", "500", "700"],
 	variable: "--font-jost",
 	display: "swap",
 });
