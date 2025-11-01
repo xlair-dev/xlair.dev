3a36556bd30dd2810d630b8b6a9e8caf9d2fcafc
n4mlz <n4mlz.dev@gmail.com>
2025-11-01 10:48:54 +0900

    :sparkles: ぼかした円を回転させる背景を追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<>
+		<div className="brand-blur-background">
 			<ObiStrip
 				height={{
 					base: "160px",
@@ -12,7 +12,7 @@ export default function Home() {
 					lg: "320px",
 				}}
 			/>
-			<main className="relative flex min-h-screen flex-col items-center justify-center px-4">
+			<main className="relative min-h-screen flex flex-col items-center justify-center px-4">
 				<div
 					className="absolute -left-20 top-0 h-screen w-auto opacity-80"
 					style={{
@@ -54,6 +54,6 @@ export default function Home() {
 					</div>
 				</div>
 			</main>
-		</>
+		</div>
 	);
 }
